06b06784efdebd4bc83ff150b732acbce840ed63
Yasser Tahiri <yasserth19@gmail.com>
2021-07-27 07:47:37 +0100

    🛠 Create the `Tailwind` Configuration

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          lighter: 'hsl(207, 73%, 52%)',
+          default: 'hsl(207, 73%, 57%)',
+          darker: 'hsl(207, 73%, 44%)',
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
