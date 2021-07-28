18dd1d4f40dfa76f6ec48907f031666510092c4d
Yasser Tahiri <yasserth19@gmail.com>
2021-07-28 01:30:05 +0100

    🎨 Create the `Service Worker` Configuration

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,10 @@
 import './src/css/tailwind.css';
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` + `Reload to display the latest version?`
+  );
+  if (answer === true) {
+    window.location.reload();
+  }
+};
