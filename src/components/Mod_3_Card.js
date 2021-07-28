488d005e334afbc348d4dac805a4232e73479d42
Yasser Tahiri <yasserth19@gmail.com>
2021-07-28 18:25:13 +0100

    ⚙ Create `Data` For Mods

---
+++ src/components/Mod_3_Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './Card';
+
+const Mod_3_Card = ({ mod_3 }) => (
+  <Card className="mb-8 shadow-2xl">
+    <p className="text-xl font-semibold">{mod_3.mod_3_Name}</p>
+    <p className="mt-6">{mod_3.content}</p>
+    <div className="flex items-center mt-8">
+      <img
+        className="w-12 h-12 mr-4 rounded-full"
+        src={mod_3.mod_3_Image}
+        alt={mod_3.mod_3_Name}
+      />
+      <div>
+        <p className="text-sm text-gray-600">{mod_3.mod_3_Title}</p>
+        
+        <p className="text-sm text-gray-600">{mod_3.mod_3_Twitter}</p>
+      </div>
+    </div>
+  </Card>
+);
+
+export default Mod_3_Card;
---
+++ src/data/Mod3-data.js
@@ -0,0 +1,31 @@
+export default [
+    {
+      title: 'Ikram',
+      content:
+        'Programs must be written for people to read, and only incidentally for machines to execute. Mod of @BnademOverFlow',
+      mod_3_Name: 'Ikram Kharbouch',
+      mod_3_Title: 'FullStack Developer',
+      mod_3_Twitter: '@_nihilistfolk',
+      mod_3_Image:
+        'https://media-exp1.licdn.com/dms/image/C4D03AQFkm8__8zmOSw/profile-displayphoto-shrink_200_200/0/1570297565700?e=1632960000&v=beta&t=wetJd9H1uZ8hHIhtjbVX6dEA350iOSzl31sykNWGKS4',
+    },
+    {
+      title: 'Rraji Abdelbari',
+      content:
+        'A programmer that write kernels in free time & 1337 Student. Mod of @BnademOverFlow',
+      mod_3_Name: 'Rraji Abdelbari',
+      mod_3_Title: '1337 Student',
+      mod_3_Twitter: '@estarossaaa_',
+      mod_3_Image:
+        'https://pbs.twimg.com/profile_images/1405909268163923972/XkemxrI5_400x400.jpg',
+    },
+    {
+      title: 'Mounir Sabiri',
+      content:
+        'Who ever stayed tuned to see his creativity blow will be granted passage to the next level. Mod of @BnademOverFlow.',
+      mod_3_Name: 'Mounir Sabiri',
+      mod_3_Title: 'Game Developer | C/C++ Dev',
+      mod_3_Twitter: '@MounirSabiri',
+      mod_3_Image: 'https://avatars.githubusercontent.com/u/54768823?v=4',
+    },
+  ];
