3e7dc23f8baeab545bb4656ee8eeeba9a75e8ed7
Yasser Tahiri <yasserth19@gmail.com>
2021-07-28 18:25:46 +0100

    🛠 Fix `Style Components`

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 const sizes = {
   default: `py-3 px-8`,
   lg: `py-4 px-12`,
-  xl: `py-5 px-16 text-lg`
+  xl: `py-5 px-16 text-lg`,
 };
 
 const Button = ({ children, className = '', size }) => {
@@ -13,10 +13,7 @@ const Button = ({ children, className = '', size }) => {
       className={`
         ${sizes[size] || sizes.default}
         ${className}
-        bg-primary
-        hover:bg-black
-        text-white
-        rounded-full
+        bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 text-white font-semibold px-6 py-3 rounded-full mr-6 
     `}
     >
       {children}
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 const Card = ({ className, children }) => (
   <div
-    className={`p-12 rounded-lg border border-solid border-gray-200 ${className}`}
-    style={{
-      boxShadow: '0 20px 15px rgba(0,0,0,.08)'
-    }}
+    className={`p-12 rounded-lg border shadow-xl border-solid border-gray-200 ${className}`}
   >
     {children}
   </div>
