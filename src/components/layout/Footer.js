ece347c572d97128c1861c7b13102216283ace7c
Yasser Tahiri <yasserth19@gmail.com>
2021-07-28 01:30:49 +0100

    🛠 Fix `Layout` and `Footer`

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -47,7 +47,7 @@ const Footer = () => (
                 <i className="flex fab fa-discord"></i>
               </button>
             </a>
-            <a href="https://github.com/BnademOverflow/BnademOverflow" target="_blank">
+            <a href="https://github.com/BnademOverflow" target="_blank">
               <button
                 className="bg-white text-gray-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-1"
                 type="button"
@@ -86,6 +86,15 @@ const Footer = () => (
                     href="#About"
                   >
                     About Us
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                    href="https://github.com/BnademOverflow/BnademOverflow/tree/Nextjs-Version"
+                    target="_blank"
+                  >
+                    NextJs Branch
                   </a>
                 </li>
                 <li>
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,92 +1,15 @@
-import { graphql, StaticQuery } from 'gatsby';
-import PropTypes from 'prop-types';
 import React from 'react';
-import Helmet from 'react-helmet';
 import Footer from './Footer';
 import Header from './Header';
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={(data) => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            {
-              name: 'description',
-              content:
-                'Community with genius people who love to be beside the computer and understand how its works.',
-            },
-            {
-              name: 'keywords',
-              content: 'ReactJS, Gatsby, BnademOverFlow',
-            },
-            { name: 'author', content: 'BnademOverFlow' },
-            {
-              name: 'og:title',
-              content: data.site.siteMetadata.title,
-            },
-            {
-              name: 'og:description',
-              content:
-                'Community with genius people who love to be beside the computer and understand how its works.',
-            },
-            { name: 'og:url', content: 'https://bnademoverflow.com/' },
-            {
-              name: 'og:image',
-              content:
-                'https://raw.githubusercontent.com/BnademOverflow/BnademOverflow-Community/main/assets/background.png',
-            },
-            { name: 'og:type', content: 'website' },
-            {
-              name: 'og:site_name',
-              content: 'BnademOverFlow',
-            },
-            { name: 'twitter:card', content: 'summary' },
-            { name: 'twitter:site', content: '@BnademOverFlow' },
-            {
-              name: 'twitter:title',
-              content: data.site.siteMetadata.title,
-            },
-            {
-              name: 'twitter:description',
-              content:
-                'Community with genius people who love to be beside the computer and understand how its works.',
-            },
-            {
-              name: 'twitter:image',
-              content:
-                'https://raw.githubusercontent.com/BnademOverflow/BnademOverflow-Community/main/assets/background.png',
-            },
-            { name: 'twitter:card', content: 'summary' },
-            { name: 'twitter:site', content: '@BnademOverFlow' },
-            { name: 'twitter:creator', content: '@BnademOverFlow' },
-            { name: 'twitter:domain', content: 'https://bnademoverflow.com/' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <>
-          <Header />
-          <main className="text-gray-900">{children}</main>
-          <Footer />
-        </>
-      </>
-    )}
-  />
-);
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="text-gray-900">{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
