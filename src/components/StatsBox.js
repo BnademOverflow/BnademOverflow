63c79dc4e433cc75873cab9014ad4d9aa8d95f75
Faissal Wahabali <fwahabali@gmail.com>
2021-10-06 02:06:01 +0100

    Countup stats ✨ (#67)
    * Stats countup when visible
    
    * reset version

--- src/components/StatsBox.js
+++ src/components/StatsBox.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
 
 const StatsBox = ({ primaryText, secondaryText }) => (
   <>
-    <p className="text-4xl lg:text-6xl font-semibold text-primary">{primaryText}</p>
+    <p className="text-4xl lg:text-6xl font-semibold text-primary">
+      <CountUp
+        end={primaryText}
+        duration={4}
+        prefix={'+'}
+        useEasing={true}
+      >
+        {({ countUpRef, start }) => (
+          <VisibilitySensor onChange={start}>
+            <span ref={countUpRef} />
+          </VisibilitySensor>
+        )}
+      </CountUp>
+    </p>
     <p className="font-semibold mb-6">{secondaryText}</p>
   </>
 );
